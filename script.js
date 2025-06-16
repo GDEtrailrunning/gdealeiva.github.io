44aa5a8910b20c9435917bbfdcc1f8b9b3f18805
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-06-16 10:26:33 -0300

    Commit weeks 16 & 17

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 16',
-  data: [16, 17, 22], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 18',
+  data: [11, 10, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 17',
-    data: [15, 20, 23], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 19',
+    data: [16, 17, 24], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -120,16 +120,5 @@ function actualizarDiasRestantes() {
   document.getElementById("Semana 2").appendChild(video3);
   document.getElementById("Semana 2").appendChild(video4);
 
-
-  audios.forEach(audio => {
-    audio.addEventListener("play", () => {
-      audios.forEach(otherAudio => {
-        if (otherAudio !== audio) {
-          otherAudio.pause();
-        }
-      });
-    });
-  });
-  
 });
 
