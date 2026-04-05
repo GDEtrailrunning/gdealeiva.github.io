b97b2ddc5e3e86c5c7357e78731b763ea058acfd
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-04-05 17:12:42 -0300

    Subo semana 9 y 10

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 7',
-    data: [9, 9, 16], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 9',
+    data: [10, 10, 19], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 8',
-    data: [9, 11, 22], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 10',
+    data: [11, 12, 23], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -43,6 +43,7 @@ const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), {
   data: dataLine1,
   options: {
     responsive: true, // Esto asegura que el gráfico se ajuste a diferentes tamaños de pantalla
+    maintainAspectRatio: false,
     scales: {
       y: {
         beginAtZero: true
@@ -55,6 +56,7 @@ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
   data: dataLine2,
   options: {
     responsive: true, // Esto asegura que el gráfico se ajuste a diferentes tamaños de pantalla
+    maintainAspectRatio: false,
     scales: {
       y: {
         beginAtZero: true
@@ -65,7 +67,7 @@ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
 
 function actualizarDiasRestantes() {
   // Fecha objetivo fija
-  const objetivoDate = new Date('2025-11-20');
+  const objetivoDate = new Date('2026-05-09');
   const currentDate = new Date();
   const timeDiff = objetivoDate - currentDate;
   const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
